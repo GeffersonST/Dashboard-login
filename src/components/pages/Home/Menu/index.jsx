4a84da906960cdf68437167f08e13315c1aee81a
GeffersonST <geffsevero@gmail.com>
2022-01-17 04:24:53 -0300

    Inserindo API

--- src/components/pages/Home/Menu/index.jsx
+++ src/components/pages/Home/Menu/index.jsx
@@ -1,9 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { MenuContainer } from "./MenuElements";
+import axios from "axios";
+
 const Menu = () => {
+  const [menuData, setMenuData] = useState([]);
+
+  useEffect(() => {
+    const getMenu = async () => {
+      await axios
+        .get("https://desafioreact.s3.amazonaws.com/menu/menu.json")
+        .then((response) => {
+          console.log(response.menus);
+          // eslint-disable-next-line no-undef
+          setMenuData(response.menus.results);
+        });
+    };
+    getMenu();
+  }, []);
+
   return (
     <>
-      <MenuContainer>OI</MenuContainer>
+      <MenuContainer>
+        <ul>
+          <h1>
+            {menuData.map((tema) => {
+              // eslint-disable-next-line no-undef
+              return <li>{tema.id}</li>;
+            })}
+          </h1>
+        </ul>
+      </MenuContainer>
     </>
   );
 };
